30673119d6f17c632eddfd38235e8f6514f8bd78
McChelson <162916835+McChelson@users.noreply.github.com>
2025-09-14 04:06:55 +0000

    Update AppContext.jsx

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    const currencySymbol = '$'
+    const currencySymbol = 'GH₵'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [doctors, setDoctors] = useState([])
